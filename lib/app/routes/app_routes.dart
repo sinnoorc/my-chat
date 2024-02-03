2f80d3c656cd2cab0c24d359258d0775234844b7
Sinnoor C <sinnoor@syoft.com>
2024-02-03 21:28:28 +0530

    added login

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -3,9 +3,11 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const home = _Paths.home;
+  static const login = _Paths.login;
 }
 
 abstract class _Paths {
   _Paths._();
   static const home = '/home';
+  static const login = '/login';
 }
